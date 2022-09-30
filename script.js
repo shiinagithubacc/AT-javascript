d24119bf9df789ebf98e6c88e99110c68e11f320
Pietro Moreno <110237893+shiinagithubacc@users.noreply.github.com>
2022-09-30 03:48:44 +0000

    AT de javascript

--- script.js
+++ script.js
@@ -3,20 +3,11 @@ let jogo = 0
 let casas = [0, 1, 2, 3, 4, 5, 6, 7, 8]
 let ContadorX = 0
 let ContadorO = 0
-let segundos = 0
-let minutos = 0
+var segundos = 0
+var minutos = 0
+let recorde = [999999, 999999]
 function selecionarCasa(event, numCasa) {
   const tile = event.target;
-  function segundo(){
-    segundos++;
-    if (segundos==60){
-      minutos++;
-      segundos=0
-      document.getElementById("minuto").innerHTML=minutos
-    }
-    document.getElementById("segundo").innerHTML=segundos
-  }
-  setInterval(function(){segundo()}, 1000)
   alert("você clicou na casa " + numCasa)
   if (document.getElementById(numCasa).innerText == "X" || document.getElementById(numCasa).innerText == "O") {
     alert("Casa já preenchida, escolha outra.")
@@ -53,6 +44,7 @@ function selecionarCasa(event, numCasa) {
       document.getElementById("8").removeAttribute("onclick")
       document.getElementById("9").removeAttribute("onclick")
       jogo = 1
+      recor()
     }
     else if ((casas[0]== "O" && casas[1] == "O" && casas[2] == "O") || (casas[3]== "O" && casas[4] == "O" && casas[5] == "O") || (casas[6]== "O" && casas[7] == "O" && casas[8] == "O") || (casas[0]== "O" && casas[3] == "O" && casas[6] == "O") || (casas[1]== "O" && casas[4] == "O" && casas[7] == "O") || (casas[2]== "O" && casas[5] == "O" && casas[8] == "O") || (casas[0]== "O" && casas[4] == "O" && casas[8] == "O") || (casas[2]== "O" && casas[4] == "O" && casas[6] == "O")) {
        if (turno <= 9){
@@ -87,4 +79,50 @@ function selecionarCasa(event, numCasa) {
       document.getElementById("9").removeAttribute("onclick")
     }
   }
+}
+function tempo(){
+    segundos++;
+    if (segundos==60){
+      minutos++;
+      segundos=0
+      document.getElementById("minuto").innerText=minutos
+    }
+    document.getElementById("segundo").innerText=segundos
+  }
+  setInterval(function(){tempo()}, 1000)
+function recor(){
+  if (minutos <= recorde[0]){
+        if (segundos < recorde[1]){
+          recorde[0] = minutos
+          recorde[1] = segundos
+          document.getElementById("record").innerText = (minutos+":"+segundos)
+        }
+      }
+}
+function reiniciar(){''
+  turno = 1
+  jogo = 0
+  segundos = 0
+  minutos = 0
+  document.getElementById("segundo").innerText = 0
+  document.getElementById("minuto").innerText = 0
+  document.getElementById("1").innerText = ""
+  document.getElementById("2").innerText = ""
+  document.getElementById("3").innerText = ""
+  document.getElementById("4").innerText = ""
+  document.getElementById("5").innerText = ""
+  document.getElementById("6").innerText = ""
+  document.getElementById("7").innerText = ""
+  document.getElementById("8").innerText = ""
+  document.getElementById("9").innerText = ""
+  document.getElementById("1").setAttribute("onclick", "selecionarCasa(event, 1)")
+  document.getElementById("2").setAttribute("onclick", "selecionarCasa(event, 2)")
+  document.getElementById("3").setAttribute("onclick", "selecionarCasa(event, 3)")
+  document.getElementById("4").setAttribute("onclick", "selecionarCasa(event, 4)")
+  document.getElementById("5").setAttribute("onclick", "selecionarCasa(event, 5)")
+  document.getElementById("6").setAttribute("onclick", "selecionarCasa(event, 6)")
+  document.getElementById("7").setAttribute("onclick", "selecionarCasa(event, 7)")
+  document.getElementById("8").setAttribute("onclick", "selecionarCasa(event, 8)")
+  document.getElementById("9").setAttribute("onclick", "selecionarCasa(event, 9)")
+  casas.splice(0, casas.length);
 }
